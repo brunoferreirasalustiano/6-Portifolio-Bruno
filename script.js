a0c39e9aa26728ea3d9f0d48074e6f0646eb0b20
brunoferreirasalustiano <brunoferreirasalustiano@gmail.com>
2026-05-24 11:32:24 -0300

    fix: adjust for vercel backend

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ if (form) {
     const timeoutId = setTimeout(() => controller.abort(), 8000); // 8000ms = 8s
 
     try {
-        const response = await fetch('https://6-portifolio-bruno-production.up.railway.app/contact', {
+        const response = await fetch('6-portifolio-bruno.vercel.app', {
             method: 'POST',
             signal: controller.signal, // Lincando o abort ao fetch
             headers: {
